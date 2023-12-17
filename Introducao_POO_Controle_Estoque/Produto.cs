3703a612d004a91fa2cda685242bc3f38618c1f1
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-17 20:18:58 -0300

    Classe produto

---
+++ Introducao_POO_Controle_Estoque/Produto.cs
@@ -0,0 +1,30 @@
+﻿namespace Introducao_POO_Controle_Estoque
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine(
+                $"Dados do produto: {Nome}, R$ {Preco}, {Quantidade} unidades, Total: R$ {ValorTotalEmEstoque()}");
+        }
+    }
+}
--- Introducao_POO_Controle_Estoque/Program.cs
+++ Introducao_POO_Controle_Estoque/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using Introducao_POO_Controle_Estoque;
+
+Console.WriteLine("Entre com os dados do produto:");
+
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantidade no estoque: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+Produto p = new Produto()
+{
+    Nome = nome,
+    Preco = preco,
+    Quantidade = quantidade
+};
+
+p.MostrarDados();
+
+Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+p.MostrarDados();
+
+Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+p.RemoverProdutos(int.Parse(Console.ReadLine()));
+p.MostrarDados();
